2890bbe2480f469c6fb9797adc129ab8a0036171
Ajie Pangestu <paangestu@gmail.com>
2021-09-20 05:47:43 +0700

    Create .eslint.json.vue

---
+++ .eslint.json.vue
@@ -0,0 +1,28 @@
+{
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:vue/vue3-recommended",
+        "standard"
+    ],
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "vue/html-indent": [
+            "error",
+            4
+        ]
+    }
+}
